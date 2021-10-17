86e5e4eb1095aee57ab45d7da2d478b541504134
Afreul kaium Joy <afreuljoy@gmail.com>
2021-10-17 20:57:28 +0600

    formetted string & Prevent auto newline added

---
+++ 6-Formatted-String.py
@@ -0,0 +1,7 @@
+num1 = 20
+num2 = 30
+
+sum = num1+ num2
+
+# string এর ভিতরে variable এবং অন্য লিখা দেখাইতে চাইলে f এবং "" দিতে হবে 
+print(f"Result is {num1} + {num2} = {sum}")
---
+++ 7-Stop-Print-newline.py
@@ -0,0 +1,4 @@
+# end="" এটি দিলেই অটো newline বন্ধ হয়ে যাবে
+
+print("Afreul Joy",end=" ")
+print("He Wants to live in Europe")
