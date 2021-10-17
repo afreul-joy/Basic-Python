eb85ae7e51f70865961dab865bada04bcf4378c0
Afreul kaium Joy <afreuljoy@gmail.com>
2021-10-17 20:10:47 +0600

    input added

---
+++ 4-Input-from-user.py
@@ -0,0 +1,12 @@
+# input একটি string ফাংশন 
+name= input("Enter Your Name : ")
+print(name)
+
+# input থেকে int/float এ কনভার্ট করে নিতে হবে 
+num1= int(input("Enter A Number : "))
+num2= float(input("Enter A Number : "))
+
+sum = num1+num2
+print(sum)
+
+
