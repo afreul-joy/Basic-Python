5fe7f3d66709e1285ab70fb4908aae39d3b5d18d
Afreul kaium Joy <afreuljoy@gmail.com>
2021-10-20 00:08:53 +0600

    continue added

---
+++ 13-continue.py
@@ -0,0 +1,11 @@
+# যখনি if এর শর্ত true হবে তখনি continue কাজ করবে
+
+# যখনি if এর শর্ত true হবে তখনি continue কাজ করবে continue মানে হচ্ছে আবার লুপ এ ফিরিয়ে দিবে মানে ওই সংখ্যাটি প্রিন্ট করবেনা
+
+for i in range (1,10):
+    if i%2==0:
+        continue
+    print(i)
+print("Ok")
+   
+    
