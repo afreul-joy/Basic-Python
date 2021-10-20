1aed3e8329c3b9cf7809e4483c955128640e760b
Afreul kaium Joy <afreuljoy@gmail.com>
2021-10-20 11:35:10 +0600

    list basic added

---
+++ 14-list.py
@@ -0,0 +1,20 @@
+sub = ['Math', 'English', 'Bangla','Physics','Chemistry',]
+
+# print(sub)
+# print(sub[1])
+# print(sub[-1])  # reverse উলটা ভাবে কাজ করবে
+
+#যদি আমি চাই 2 নাম্বার index থেকে প্রিন্ট করতে তাহলে তাহলে শুরুতে(2: 4)
+# print(sub[2:4])
+
+#in অপারেটর এর কাজ হল যদি array/list এর মাঝে থাকে তবে true দিবে
+# print("Biology" in sub)
+
+#not in অপারেটর এর কাজ হল যদি array/list এর মাঝে যদি না থাকে তবে true দিবে
+# print("Biology" not in sub)
+
+# আগের array/list এর সাথে নতুন array/list যোগ করা যায়
+print(sub+["ICT", 55])
+# print(len(sub))
+
+
