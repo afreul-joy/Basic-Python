f2d65d5c25b621ba768b4742489e3789eaa5006e
AFreul kaium Joy <afreuljoy@gmail.com>
2021-12-06 19:42:15 +0600

    python

---
+++ 28_Class & Constructor & Method .py
@@ -0,0 +1,15 @@
+class Student :
+  #studentRoll = ""  #property
+  #studentGpa = ""
+  
+  def  __init__(self,roll,gpa):  #constructor
+    self.studentRoll = roll 
+    self.studentGpa = gpa
+
+  def display(self):   #normal method
+    print(f"Roll = {self.studentRoll}, Gpa = {self.studentGpa}") 
+
+rahim = Student("101","3.70")
+rahim.display()
+
+
